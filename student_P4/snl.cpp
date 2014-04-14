0eddd99b481e15bc112c6e81698fbb7805a5a3fe
Haowei Yu <yuhaowei@dc3nep72.dc.umich.edu>
2014-04-14 17:05:25 -0400

    snl update

--- student_P4/snl.cpp
+++ student_P4/snl.cpp
@@ -14,6 +14,80 @@ Status Operators::SNL(const string& result,           // Output relation name
   cout << "Algorithm: Simple NL Join" << endl;
 
   /* Your solution goes here */
+  Status Scanattr1;
+  HeapFileScan *FindMatch1;
+  FindMatch1 = new HeapFileScan(attrDesc1->relname, Scanattr1);
+
+  if (Scanattr1 != OK){
+  	return Scanattr1;
+  }
+
+
+
+  Status StatusAfterOpenResult;
+  HeapFile resultHeapFile(result, StatusAfterOpenResult);
+  if ( StatusAfterOpenResult != OK){
+  	return StatusAfterOpenResult;
+  }
+
+  RID outRid1; 
+  Record rec1; 
+  Status statusGetNext1; 
+
+  while((statusGetNext1 = FindMatch1->scanNext(outRid1,rec1)) ==OK){
+  	Status  Scanattr2;
+  	HeapFileScan *FindMatch2;
+  	FindMatch2 = new HeapFileScan(attrDesc2->relname, Scanattr2);
+  	if (Scanattr2 != OK){
+  		return Scanattr2;
+  	}
+
+  	RID outRid2;
+  	Record rec2;
+  	Status statusGetNext2;
+  	while ((statusGetNext2 = FindMatch2->scanNext(outRid2,rec2)) ==OK){
+  		if (( (op == LT || op==LTE) && matchRec(rec1,rec2,attrDesc1,attrDesc2) < 0) ||
+  			(matchRec(rec1,rec2,attrDesc1,attrDesc2) == 0 && (op == LTE || op == GTE)) ||
+  			(matchRec(rec1,rec2,attrDesc1,attrDesc2) > 0 && (op == GT || op == GTE)) 
+  		){
+  			int i;
+        			Record resultRec;
+        			RID dummy;// no use
+        			char* data = new char[reclen];
+        			char* temp = data;
+        			for( i = 0 ; i < projCnt ; i++)
+        			{
+        				if ( strcmp(projNames[i].relname, attrDesc1.relname) ){
+            					memcpy(temp,(char*)rec1.data+projNames[i].attrOffset,projNames[i].attrLen);
+            					temp = temp + projNames[i].attrLen;
+            				}
+            				else {
+            					memcpy(temp,(char*)rec2.data+projNames[i].attrOffset,projNames[i].attrLen);
+            					temp = temp + projNames[i].attrLen;
+            				}
+        			}
+        			resultRec.data = (void*) data;
+        			resultRec.length = reclen;
+        			Status statusInsertRec;
+        			statusInsertRec = resultHeapFile.insertRecord(resultRec,dummy);
+        			if(statusInsertRec != OK)
+        			{
+            				return statusInsertRec;
+        			}
+  		}
+  		
+  	}
+
+  	if(statusGetNext2 != FILEEOF){
+        		return statusGetNext2;
+  	}
+  	delete FindMatch2;
+  }
+
+  if(statusGetNext1 != FILEEOF){
+        return statusGetNext1;
+  }
+  delete FindMatch1;
 
   return OK;
 }
